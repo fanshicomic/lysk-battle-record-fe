1fcaf8ca05f9df6ea8eb1148596438fcf313f638
fanshicomic <fanshicomic@gmail.com>
2025-05-28 01:00:22 +0800

    feat: partner selection optimize

---
+++ util.js
@@ -0,0 +1,15 @@
+export async function apiPost(endpoint, data) {
+    const res = await fetch(`${API_BASE}${endpoint}`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+    });
+    return res.json();
+}
+
+export async function apiGet(endpoint, params = {}) {
+    const url = new URL(`${API_BASE}${endpoint}`);
+    Object.entries(params).forEach(([key, val]) => url.searchParams.append(key, val));
+    const res = await fetch(url.toString());
+    return res.json();
+}
