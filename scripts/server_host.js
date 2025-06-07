7c55de86d4e12b2fd25b97fa5e926bede0adace0
fanshicomic <fanshicomic@gmail.com>
2025-06-07 11:49:44 +0800

    feat: test host func working fine

--- scripts/server_host.js
+++ scripts/server_host.js
@@ -1,2 +1,7 @@
-export const API_BASE = 'https://lysk-battle-record-426168069563.asia-southeast1.run.app/';
-// export const API_BASE = 'http://192.168.0.139:8080/';
+let API_BASE;
+if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
+    API_BASE = 'http://192.168.0.139:8080/';
+} else {
+    API_BASE = 'https://lysk-battle-record-426168069563.asia-southeast1.run.app/';
+}
+export { API_BASE };
