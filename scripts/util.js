fb40604ace1bb3222d9e1efbd162058e3cdafdb0
fanshicomic <fanshicomic@gmail.com>
2025-06-17 22:26:30 +0900

    feat: do not update google tag import

--- scripts/util.js
+++ scripts/util.js
@@ -1,4 +1,4 @@
-import { API_BASE } from "./server_host.js?v=1750166440";
+import { API_BASE } from "./server_host.js?v=1750166732";
 
 export async function apiPost(endpoint, data) {
     const res = await fetch(`${API_BASE}${endpoint}`, {
