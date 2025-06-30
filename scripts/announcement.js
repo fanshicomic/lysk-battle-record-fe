a71a6b03ca9d222ee3b72d4ab92a1eeb8e2e74e4
fanshicomic <fanshicomic@gmail.com>
2025-07-01 01:18:36 +0800

    feat: add form-reset button

--- scripts/announcement.js
+++ scripts/announcement.js
@@ -1,5 +1,5 @@
 export function showAnnouncement() {
-    const announcementKey = "1.2.4"
+    const announcementKey = "1.2.5"
     if (localStorage.getItem(announcementKey)) return;
     localStorage.setItem(announcementKey, '1');
 
@@ -30,14 +30,15 @@ function getAnnouncementBody() {
             欢迎使用深空面板助手 :)
             库中的面板会用于正在开发的数据分析功能，感谢你的每一条记录分享~
             更多功能如筛选、排序、截图识别也在开发进程中，敬请期待
+            小程序开发进度：~75%
             公告可前往本助手首页右上角重新查看`
 }
 
 function getAnnouncementLatestUpdates() {
     return `最新更新
+            - 2025-07-01：增加重置表单按钮
             - 2025-06-25：增加坠浪四星日卡
             - 2025-06-17：支持搭档利莫里亚海神
             - 2025-06-10：表单阶数显示优化
-            - 2025-06-09：新增系统公告功能+防御/生命搭档上传数据验证
-            - 2025-06-07：更具体的上传数据验证`
+            - 2025-06-09：新增系统公告功能+防御/生命搭档上传数据验证`
 }
