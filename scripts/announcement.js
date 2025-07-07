76a247abdc345bad7e679846a811885b803f0782
fanshicomic <fanshicomic@gmail.com>
2025-07-07 17:21:48 +0800

    feat: update announcement and version

--- scripts/announcement.js
+++ scripts/announcement.js
@@ -1,5 +1,5 @@
 export function showAnnouncement() {
-    const announcementKey = "1.3.0"
+    const announcementKey = "2.0.1"
     if (localStorage.getItem(announcementKey)) return;
     localStorage.setItem(announcementKey, '1');
 
@@ -28,18 +28,18 @@ function getAnnouncementBody() {
     return `猎人小姐你好!
     
             欢迎使用深空面板助手 :)
+            
+            【微信小程序现已上线：深空面板助手】
+            
             库中的面板会用于正在开发的数据分析功能，感谢你的每一条记录分享~
-            更多功能如筛选、排序、截图识别也在开发进程中，敬请期待
-            小程序开发进度：~75%
+            更多功能也在开发进程中，敬请期待
             公告可前往本助手首页右上角重新查看`
 }
 
 function getAnnouncementLatestUpdates() {
     return `最新更新
-            - 2025-07-01：支持新的关卡模式（预计未来非开放轨道也会增加波动模式，所以调整为所有关卡都支持双模式。）
-            - 2025-07-01：增加重置表单按钮
-            - 2025-06-25：增加坠浪四星日卡
-            - 2025-06-17：支持搭档利莫里亚海神
-            - 2025-06-10：表单阶数显示优化
-            - 2025-06-09：新增系统公告功能+防御/生命搭档上传数据验证`
+            - 2025-07-07：微信小程序正式上线！搜：深空面板助手
+            - 2025-07-07：支持识图功能
+            - 2025-07-01：支持新的关卡模式
+            - 2025-07-01：增加重置表单按钮`
 }
